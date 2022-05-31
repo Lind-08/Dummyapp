a719eab4507a8110e5b49404c19f48667d443ed8
lind <lind8584@gmail.com>
2022-05-31 22:03:03 +0300

    Добавьте файлы проекта.

---
+++ dummyapp.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <winspool.h>
+
+int wmain()
+{
+    DWORD length = 0;
+    GetDefaultPrinter(nullptr, &length);
+    if (length != 0)
+    {
+        wchar_t* buffer = new wchar_t[length];
+        if (!GetDefaultPrinter(buffer, &length))
+        {   
+            std::cout << "Error!" << std::endl;
+        }
+        std::wcout << std::wstring(buffer) << std::endl;
+    }
+}
